55a92d76be1ff5933f8a8b7b0baee5d8f8d8c096
Cy90sure <maria.pozniak2013@gmail.com>
2024-05-31 12:46:00 +0300

    Added view category_list

--- books/views.py
+++ books/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-from .models import Recipe
+from .models import Recipe, Category
 
 def main(request):
     latest_recipes = Recipe.objects.all().order_by('-created_at')[:5]
     return render(request, 'main.html', {'recipes': latest_recipes})
+
+def category_list(request):
+    categories = Category.objects.all()
+    categories_with_count = []
+    for category in categories:
+        recipe_count = Recipe.objects.filter(category=category).count()
+        categories_with_count.append({'category': category, 'count': recipe_count})
+    return render(request, 'category_list.html', {'categories_with_count': categories_with_count})
